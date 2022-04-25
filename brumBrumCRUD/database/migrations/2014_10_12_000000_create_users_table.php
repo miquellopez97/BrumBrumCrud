455fafbe9aafc1a642cf1a4453ff6e19957f0245
miquellopez97 <miquel.lopez97@gmail.com>
2022-04-25 20:35:36 +0200

    firts commit

---
+++ brumBrumCRUD/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('password');
+            $table->string('rol')->nullable();
+            $table->string('detail')->nullable();
+            $table->string('otherInformation')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('googleID')->nullable();
+            $table->rememberToken();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
