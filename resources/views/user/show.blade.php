444c14fad4fad03f53056c187985631d6b7163f7
miquellopez97 <miquel.lopez97@gmail.com>
2022-05-13 11:36:13 +0200

    boton

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -7,7 +7,7 @@
                 <h2>Show User</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('user.index') }}"> Back</a>
+                <a class="btn btn-primary" href="{{ route('user.index') }}">Lista usuarios</a>
             </div>
         </div>
     </div>
