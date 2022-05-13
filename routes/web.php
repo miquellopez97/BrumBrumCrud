6e7a8ec45ff525643210e714568db82561f40632
miquellopez97 <miquel.lopez97@gmail.com>
2022-05-13 11:30:04 +0200

    ruta

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\UserAuthController;
 
 Route::resource('user', ViewsController::class)->middleware('isLogged');
 
-Route::get('login',[UserAuthController::class, 'login'])->middleware('AlreadyLoggedIn');
+Route::get('/',[UserAuthController::class, 'login'])->middleware('AlreadyLoggedIn');
 Route::post('check',[UserAuthController::class, 'check'])->name('auth.check');
 Route::get('profile',[UserAuthController::class, 'profile'])->middleware('isLogged');
 Route::get('logout',[UserAuthController::class, 'logout']);
